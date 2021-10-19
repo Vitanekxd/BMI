63597703b1ea565f8c5a08c9398f697270acb22c
Vít Odehnal <odehnal11@meail.cz>
2021-10-19 11:37:58 +0200

    hotovo

--- BMI/Program.cs
+++ BMI/Program.cs
@@ -6,7 +6,26 @@ namespace BMI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string a;
+            string b;
+            double c;
+
+            Console.Clear();
+            Console.WriteLine("Klakulačka BMI");
+            Console.WriteLine("--------------");
+
+            Console.WriteLine("zadej výšku(cm):");
+            a = Console.ReadLine();
+            int vyska = int.Parse(a);
+
+            Console.WriteLine("Zadej hmotnost: ");
+            b = Console.ReadLine();
+            int hmotnost = int.Parse(b);
+
+            c = (vyska / 100) * (vyska / 100);
+
+            Console.WriteLine("Tvoje BMI je " + c);
+            Console.ReadLine();
         }
     }
 }
